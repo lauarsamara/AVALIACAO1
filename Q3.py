b6de295721588d21b8da132f208db708169b2a8d
lauarsamara <59751743+lauarsamara@users.noreply.github.com>
2020-01-15 13:55:20 -0300

    Add files via upload

---
+++ Q3.py
@@ -0,0 +1,7 @@
+3) Quais cuidados devem ser observados ao capturar dados de um site?
+
+Ao capturar dados em um site deve-se certificar que:
+
+*O conteúdo capturado não possui direitos autorais, 
+*O scraping não onera o serviço do site, e 
+*O scraper não viola os termos de uso do site e nem coleta informações sensíveis.
