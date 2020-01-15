57ae7c1e6d13b41d1351d39980a629c8f45ab1cc
lauarsamara <59751743+lauarsamara@users.noreply.github.com>
2020-01-15 14:18:08 -0300

    Add files via upload

---
+++ Q5.py
@@ -0,0 +1,6 @@
+5) Você diria que bots ou crawlers são programas facilmente paralelizáveis? Se sim, explique como isso seria implementado dando um exemplo.
+
+A tarefa geral pode ser dividida em subtarefas, sendo essas totalmente independentes umas das outras, 
+não havendo necessidade de ordem na execução e sem troca de informações entre as subtarefas. 
+Portanto, são aplicações mais fáceis de paralelizar.
+
